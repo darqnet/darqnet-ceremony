5c55204c61f6c8d954cfc4d5ad7f36b8f8d0ac97
Joel Thorstensson <joel@oed.earth>
2021-09-24 21:50:41 +0100

    Update to use Ceramic

--- ceremony.js
+++ ceremony.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
-const MuPort = require('muport-core')
-const Box = require('3box')
+const { DID } = require('dids')
+const { CeramicClient } = require('@ceramicnetwork/http-client')
+const { TileDocument } = require('@ceramicnetwork/stream-tile')
+const { Ed25519Provider } = require('key-did-provider-ed25519')
+const KeyResolver = require('key-did-resolver')
 const seedsplit = require('seedsplit')
 const bip39 = require('bip39')
 
+const API_URL = 'https://ceramic-private.3boxlabs.com'
 
 async function all () {
   const mnemonic = bip39.generateMnemonic()
-  const myq = await MuPort.newIdentity({ name: 'darqnet' }, null, {
-    mnemonic
-  })
+  const seed = new Uint8Array(bip39.mnemonicToSeedSync(mnemonic).slice(0, 32))
+  const provider = new Ed25519Provider(seed)
+  const did = new DID({ provider, resolver: KeyResolver.getResolver() })
+  await did.authenticate()
+
 
   const c = 'Feel into what the darqnet is and condense it into a sentence.\n'
   let C = []
@@ -19,7 +25,7 @@ async function all () {
   const b = 'What is your intention to bring forth into darqnet?\n'
   let B = []
 
-  const numQnetians = 7
+  const numQnetians = 2
   //const numQnetians = 1
   for (let i = 0; i < numQnetians; i++) {
     console.log('\033[2J');
@@ -29,12 +35,17 @@ async function all () {
   }
 
   console.log('\033[2J');
-  const box = new Box(myq)
 
-  await box._load()
-  await box.private.set('intentianalIntuisions', { A, B, C })
+  const ceramic = new CeramicClient(API_URL)
+  ceramic.did = did
+  const doc = await TileDocument.create(ceramic, null, { deterministic: true }, { anchor: false, publish: false })
 
-  writeAddress(box._rootStore.address.toString())
+  await doc.update({
+    a, b, c,
+    A, B, C
+  })
+
+  writeStreamId(doc.id.toString())
 
   const shards = await seedsplit.split(mnemonic, 4, 3)
 
@@ -56,14 +67,14 @@ function prompt(question) {
     const { stdin, stdout } = process
 
     stdin.resume()
-    stdout.write(question)
+    stdout.write('\n' + question)
 
     stdin.on('data', data => resolve(data.toString().trim()))
     stdin.on('error', err => reject(err))
   })
 }
 
-function writeAddress (address) {
+function writeStreamId (address) {
   var fs = require('fs');
   fs.writeFile('darqnet-root', address, function(err) {
     if(err) {
